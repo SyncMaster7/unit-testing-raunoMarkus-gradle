527c01c84dd0e43ade577f29d982713ae4653336
Kermo Keerma <32697937+SyncMaster7@users.noreply.github.com>
2023-01-14 12:18:10 +0200

    Latest update

--- src/test/java/raunoMarkus/letterCountClassTest.java
+++ src/test/java/raunoMarkus/letterCountClassTest.java
@@ -1,4 +1,16 @@
+package raunoMarkus;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class letterCountClassTest {
-  
+
+    @Test
+    void letterCount() {
+
+        Object resultValue[] = [0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        letterCountClass lcc = new letterCountClass();
+        assertEquals(resultValue, letterCountClass.letterCount("hello"));
+    }
 }
